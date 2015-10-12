e69a08461e82d1fa5f2c543f2110fec79679edd3
Matthew Wright <matthew@smallhelm.com>
2015-10-12 09:15:08 -0600

    readme and always return the css string

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
 # css-to-css.js
 Compile css into a requireable css.js file that is also safe to require on the server.
 
+## How to use it
+```js
+var cssToCssJs = require("css-to-css.js");
+
+var js = cssToCssJs("body {color: blue;}");
+```
+Which returns
+```js
+!function(e){if("undefined"==typeof document)return e;var t=document,n="CSS_TO_CSSJS";if(t[n]=t[n]||{},!t[n][e]){t[n][e]=!0;var r=t.createElement("style");return r.setAttribute("type","text/css"),"textContent"in r?r.textContent=e:r.styleSheet.cssText=e,t.getElementsByTagName("head")[0].appendChild(r),e}}("body {color: blue;}");
+```
+When you execute that javascript:
+
+In the browser
+ * injects the css into the `<head>`
+ * returns the css code as a string
+
+On the server
+ * returns the css code as a string
+
+Typically you save the javascript to a file then you can `require` it using browserify/node.
+
+## Install
+```sh
+$ npm install --save css-to-css.js
+```
+
 ## License
 MIT
--- inject-css-fn.template.js
+++ inject-css-fn.template.js
@@ -1,30 +1,29 @@
 (function(css){
-	if(typeof document === 'undefined') {
-		//not in the browser
-		return;
-	}
-	var d = document;
+  if(typeof document === 'undefined') {
+    //not in the browser
+    return css;
+  }
+  var d = document;
 
-
-	//keep track of css code that's allready been inserted
+  //keep track of css code that's allready been inserted
   var key = "CSS_TO_CSSJS"; 
-	d[key] = d[key] || {};
-	if (d[key][css]){
-		return;
-	}
-	d[key][css] = true;
+  d[key] = d[key] || {};
+  if (d[key][css]){
+    return;
+  }
+  d[key][css] = true;
 
+  //inject a style tag with the css
+  var elem = d.createElement('style');
+  elem.setAttribute('type', 'text/css');
 
-	//inject a style tag with the css
-	var elem = d.createElement('style');
-	elem.setAttribute('type', 'text/css');
+  if ('textContent' in elem) {
+    elem.textContent = css;
+  } else {
+    elem.styleSheet.cssText = css;
+  }
 
-	if ('textContent' in elem) {
-		elem.textContent = css;
-	} else {
-		elem.styleSheet.cssText = css;
-	}
+  d.getElementsByTagName('head')[0].appendChild(elem);
 
-	d.getElementsByTagName('head')[0].appendChild(elem);
-
+  return css;
 }("INPUT_CSS_HERE"));
